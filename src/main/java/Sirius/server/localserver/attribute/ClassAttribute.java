448b3533b15684c707daaa639dcf6abd83327f29
Martin Scholl <martin.scholl@cismet.de>
2010-12-07 12:25:44 +0000

    [issue:1769] added getOptions() for ClassAttributes and according JUnit test

--- src/main/java/Sirius/server/localserver/attribute/ClassAttribute.java
+++ src/main/java/Sirius/server/localserver/attribute/ClassAttribute.java
@@ -9,6 +9,11 @@ package Sirius.server.localserver.attribute;
 
 import Sirius.server.newuser.permission.Policy;
 
+import org.apache.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * DOCUMENT ME!
  *
@@ -18,7 +23,10 @@ public class ClassAttribute extends Attribute implements java.io.Serializable {
 
     //~ Static fields/initializers ---------------------------------------------
 
-    public static final String HISTORY_ENABLED = "history_enabled"; // NOI18N
+    public static final String HISTORY_ENABLED = "history_enabled";    // NOI18N
+    public static final String HISTORY_OPTION_ANONYMOUS = "anonymous"; // NOI18N
+
+    private static final transient Logger LOG = Logger.getLogger(ClassAttribute.class);
 
     //~ Instance fields --------------------------------------------------------
 
@@ -75,5 +83,43 @@ public class ClassAttribute extends Attribute implements java.io.Serializable {
      */
     public final void setTypeID(final int typeID) {
         this.typeID = typeID;
+    }
+
+    /**
+     * This method tries to parse options from the value of the class attribute. Option map is built as follows:
+     *
+     * <ul>
+     *   <li>Arbitrary options can be given separated by ",+" (consecutive commas)</li>
+     *   <li>An option can be a KV-Pair, separated by "=+" (consecutive equality signs)</li>
+     *   <li>If option is not KV-Pair, option value map shall contain an empty string for the option</li>
+     *   <li>Options are trimmed</li>
+     * </ul>
+     *
+     * @return  an option key value map, never null.
+     */
+    public Map<String, String> getOptions() {
+        final Map<String, String> options = new HashMap<String, String>(3, 0.8f);
+
+        if (value instanceof String) {
+            final String[] optionArray = ((String)value).split(",+");        // NOI18N
+            for (final String optionToken : optionArray) {
+                final String[] optionKV = optionToken.trim().split("=+", 2); // NOI18N
+                final String optionKey = optionKV[0].trim();
+                final String optionValue;
+                if (optionKV.length == 2) {
+                    optionValue = optionKV[1].trim();
+                } else {
+                    optionValue = "";
+                }
+
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("found option key (" + optionKey + ") and option value (" + optionValue + ")"); // NOI18N
+                }
+
+                options.put(optionKey, optionValue);
+            }
+        }
+
+        return options;
     }
 }
---
+++ src/test/java/Sirius/server/localserver/attribute/ClassAttributeTest.java
@@ -0,0 +1,204 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package Sirius.server.localserver.attribute;
+
+import java.util.Properties;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   martin.scholl@cismet.de
+ * @version  $Revision$, $Date$
+ */
+public class ClassAttributeTest {
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new ClassAttributeTest object.
+     */
+    public ClassAttributeTest() {
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @throws  Exception  DOCUMENT ME!
+     */
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        final Properties p = new Properties();
+        p.put("log4j.appender.Remote", "org.apache.log4j.net.SocketAppender");
+        p.put("log4j.appender.Remote.remoteHost", "localhost");
+        p.put("log4j.appender.Remote.port", "4445");
+        p.put("log4j.appender.Remote.locationInfo", "true");
+        p.put("log4j.rootLogger", "ALL,Remote");
+        org.apache.log4j.PropertyConfigurator.configure(p);
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @throws  Exception  DOCUMENT ME!
+     */
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    /**
+     * DOCUMENT ME!
+     */
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * DOCUMENT ME!
+     */
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    private String getCurrentMethodName() {
+        return new Throwable().getStackTrace()[1].getMethodName();
+    }
+
+    /**
+     * DOCUMENT ME!
+     */
+    @Test
+    public void testGetOptions() {
+        System.out.println("TEST " + getCurrentMethodName());
+
+        final ClassAttribute attr = new ClassAttribute(null, 0, null, 0, null);
+
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertTrue("options not empty", attr.getOptions().isEmpty());
+
+        attr.setValue("keyA");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly one option present", 1, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "", attr.getOptions().get("keyA"));
+
+        attr.setValue("keyA, keyB, keyC");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly three options present", 3, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "", attr.getOptions().get("keyA"));
+        assertEquals("invalid option kv pair", "", attr.getOptions().get("keyB"));
+        assertEquals("invalid option kv pair", "", attr.getOptions().get("keyC"));
+
+        attr.setValue("keyA=valueA");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly one option present", 1, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "valueA", attr.getOptions().get("keyA"));
+
+        attr.setValue("keyA=");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly one option present", 1, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "", attr.getOptions().get("keyA"));
+
+        attr.setValue("keyA=   ");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly one option present", 1, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "", attr.getOptions().get("keyA"));
+
+        attr.setValue("keyA =valueA");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly one option present", 1, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "valueA", attr.getOptions().get("keyA"));
+
+        attr.setValue("keyA= valueA");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly one option present", 1, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "valueA", attr.getOptions().get("keyA"));
+
+        attr.setValue("keyA = valueA");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly one option present", 1, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "valueA", attr.getOptions().get("keyA"));
+
+        attr.setValue(" keyA = valueA ");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly one option present", 1, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "valueA", attr.getOptions().get("keyA"));
+
+        attr.setValue("keyA         =valueA       ");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly one option present", 1, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "valueA", attr.getOptions().get("keyA"));
+
+        attr.setValue("keyA         ==       ");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly one option present", 1, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "", attr.getOptions().get("keyA"));
+
+        attr.setValue("keyA         = =       ");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly one option present", 1, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "=", attr.getOptions().get("keyA"));
+
+        attr.setValue("keyA         =    ===valueA");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly one option present", 1, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "===valueA", attr.getOptions().get("keyA"));
+
+        attr.setValue("keyA         =valueA  ===valueA");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly one option present", 1, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "valueA  ===valueA", attr.getOptions().get("keyA"));
+
+        attr.setValue("  keyA   ,   keyB=valueB");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly two options present", 2, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "", attr.getOptions().get("keyA"));
+        assertEquals("invalid option kv pair", "valueB", attr.getOptions().get("keyB"));
+
+        attr.setValue("keyA   ,    keyB   =    valueB    ,   keyC");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly three options present", 3, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "", attr.getOptions().get("keyA"));
+        assertEquals("invalid option kv pair", "valueB", attr.getOptions().get("keyB"));
+        assertEquals("invalid option kv pair", "", attr.getOptions().get("keyC"));
+
+        attr.setValue("keyA = valueA, keyB = valueB");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly two options present", 2, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "valueA", attr.getOptions().get("keyA"));
+        assertEquals("invalid option kv pair", "valueB", attr.getOptions().get("keyB"));
+
+        attr.setValue("keyA = valueA , key B , keyC = valueC");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly three options present", 3, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "valueA", attr.getOptions().get("keyA"));
+        assertEquals("invalid option kv pair", "", attr.getOptions().get("key B"));
+        assertEquals("invalid option kv pair", "valueC", attr.getOptions().get("keyC"));
+
+        attr.setValue("keyA = valueA, keyB = valueB, keyC = valueC, keyD = valueD, keyE = valueE");
+        assertNotNull("class attr option map shall never be null", attr.getOptions());
+        assertEquals("not exactly five options present", 5, attr.getOptions().size());
+        assertEquals("invalid option kv pair", "valueA", attr.getOptions().get("keyA"));
+        assertEquals("invalid option kv pair", "valueB", attr.getOptions().get("keyB"));
+        assertEquals("invalid option kv pair", "valueC", attr.getOptions().get("keyC"));
+        assertEquals("invalid option kv pair", "valueD", attr.getOptions().get("keyD"));
+        assertEquals("invalid option kv pair", "valueE", attr.getOptions().get("keyE"));
+    }
+}
