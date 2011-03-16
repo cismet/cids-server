503c011cfc4ae0caf3f9f6dd162d29ba837ef0a4
Thorsten Hell <thorsten.hell@cismet.de>
2011-03-16 17:54:54 +0000

    [issue:1690] Multi SRS Geo Search (neue BuiltinSuche im Server)

--- src/main/java/Sirius/server/search/builtin/GeoSearch.java
+++ src/main/java/Sirius/server/search/builtin/GeoSearch.java
@@ -1,10 +1,10 @@
 /***************************************************
-*
-* cismet GmbH, Saarbruecken, Germany
-*
-*              ... and it just works.
-*
-****************************************************/
+ *
+ * cismet GmbH, Saarbruecken, Germany
+ *
+ *              ... and it just works.
+ *
+ ****************************************************/
 /*
  *  Copyright (C) 2010 thorsten
  *
@@ -51,24 +51,22 @@ import de.cismet.tools.collections.MultiMap;
 public class GeoSearch extends CidsServerSearch {
 
     //~ Instance fields --------------------------------------------------------
-
-    String searchGeometry = null;
+    Geometry searchGeometry = null;
 
     //~ Constructors -----------------------------------------------------------
-
     /**
      * Creates a new GeoSearch object.
      *
      * @param  searchGeometry  DOCUMENT ME!
      */
     public GeoSearch(final Geometry searchGeometry) {
-        this.searchGeometry = searchGeometry.toText();
+        this.searchGeometry = searchGeometry;
     }
 
     //~ Methods ----------------------------------------------------------------
-
     @Override
     public Collection performServerSearch() {
+        final ArrayList<Node> aln = new ArrayList<Node>();
         try {
             getLog().info("geosearch started");
 
@@ -76,49 +74,48 @@ public class GeoSearch extends CidsServerSearch {
 
 //          "select class_id,object_id,name from GEOSUCHE where class_id in <cidsClassesInStatement> and geo_field && GeometryFromText('SRID=-1;<cidsSearchGeometryWKT>') and intersects(geo_field,GeometryFromText('SRID=-1;<cidsSearchGeometryWKT>'))";
             final String sql = "WITH recursive derived_index(ocid,oid,acid,aid,depth) AS "
-                        + "( "
-                        + "SELECT class_id,object_id,cast (NULL AS int), cast (NULL AS int),0 "
-                        + "FROM GEOSUCHE WHERE class_id IN"
-                        + "( "
-                        + "WITH recursive derived_child(father,child,depth) AS ( "
-                        + "SELECT father,father,0 FROM cs_class_hierarchy WHERE father in <cidsClassesInStatement> "
-                        + "UNION ALL "
-                        + "SELECT ch.father,ch.child,dc.depth+1  FROM derived_child dc,cs_class_hierarchy ch WHERE ch.father=dc.child) "
-                        + "SELECT DISTINCT father FROM derived_child LIMIT 100 "
-                        +
-
-                        ") "
-                        + "AND geo_field && GeometryFromText('SRID=-1;<cidsSearchGeometryWKT>') AND intersects(geo_field,GeometryFromText('SRID=-1;<cidsSearchGeometryWKT>')) "
-                        + "UNION ALL "
-                        + "SELECT aam.class_id,aam.object_id, aam.attr_class_id, aam.attr_object_id,di.depth+1 FROM cs_all_attr_mapping aam,derived_index di WHERE aam.attr_class_id=di.ocid AND aam.attr_object_id=di.oid"
-                        + ") "
-                        + "SELECT ocid,oid FROM derived_index WHERE ocid in <cidsClassesInStatement> LIMIT 1000 ";
+                    + "( "
+                    + "SELECT class_id,object_id,cast (NULL AS int), cast (NULL AS int),0 "
+                    + "FROM GEOSUCHE WHERE class_id IN"
+                    + "( "
+                    + "WITH recursive derived_child(father,child,depth) AS ( "
+                    + "SELECT father,father,0 FROM cs_class_hierarchy WHERE father in <cidsClassesInStatement> "
+                    + "UNION ALL "
+                    + "SELECT ch.father,ch.child,dc.depth+1  FROM derived_child dc,cs_class_hierarchy ch WHERE ch.father=dc.child) "
+                    + "SELECT DISTINCT father FROM derived_child LIMIT 100 "
+                    + ") "
+                    + "AND geo_field && GeometryFromText('SRID=<cidsSearchGeometrySRID>;<cidsSearchGeometryWKT>') AND intersects(geo_field,GeometryFromText('SRID=<cidsSearchGeometrySRID>;<cidsSearchGeometryWKT>')) "
+                    + "UNION ALL "
+                    + "SELECT aam.class_id,aam.object_id, aam.attr_class_id, aam.attr_object_id,di.depth+1 FROM cs_all_attr_mapping aam,derived_index di WHERE aam.attr_class_id=di.ocid AND aam.attr_object_id=di.oid"
+                    + ") "
+                    + "SELECT ocid,oid FROM derived_index WHERE ocid in <cidsClassesInStatement> LIMIT 1000 ";
 
             // Deppensuche sequentiell
             final HashSet keyset = new HashSet(getActiveLoaclServers().keySet());
+            final String cidsSearchGeometryWKT = searchGeometry.toText();
+            final String sridString = Integer.toString(searchGeometry.getSRID());
 
-            final ArrayList<Node> aln = new ArrayList<Node>();
-
-            final String cidsSearchGeometryWKT = searchGeometry;
             for (final Object key : keyset) {
-                final MetaService ms = (MetaService)getActiveLoaclServers().get(key);
-                final String classesInStatement = getClassesInSnippetsPerDomain().get((String)key);
-                final String sqlStatement = sql.replaceAll("<cidsClassesInStatement>", classesInStatement)
-                            .replaceAll("<cidsSearchGeometryWKT>", cidsSearchGeometryWKT);
+                final MetaService ms = (MetaService) getActiveLoaclServers().get(key);
+                final String classesInStatement = getClassesInSnippetsPerDomain().get((String) key);
+                getLog().fatal("cidsClassesInStatement=" + classesInStatement);
+                getLog().fatal("cidsSearchGeometryWKT=" + cidsSearchGeometryWKT);
+                getLog().fatal("cidsSearchGeometrySRID=" + sridString);
+                final String sqlStatement = sql.replaceAll("<cidsClassesInStatement>", classesInStatement).replaceAll("<cidsSearchGeometryWKT>", cidsSearchGeometryWKT).replaceAll("<cidsSearchGeometrySRID>", sridString);
                 getLog().info("geosearch: " + sqlStatement);
                 final ArrayList<ArrayList> result = ms.performCustomSearch(sqlStatement);
                 for (final ArrayList al : result) {
-                    final int cid = (Integer)al.get(0);
-                    final int oid = (Integer)al.get(1);
+                    final int cid = (Integer) al.get(0);
+                    final int oid = (Integer) al.get(1);
                     final String name = null; // (String) al.get(2);
-                    final MetaObjectNode mon = new MetaObjectNode((String)key, oid, cid);
+                    final MetaObjectNode mon = new MetaObjectNode((String) key, oid, cid);
                     aln.add(mon);
                 }
             }
             return aln;
         } catch (Exception e) {
-            getLog().fatal("Problem", e);
-            return null;
+            getLog().error("Problem during GEOSEARCH", e);
+            return aln;
         }
     }
 }
