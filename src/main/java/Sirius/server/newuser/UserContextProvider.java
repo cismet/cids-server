18f114a784e713fef8f37b0a599f05bccc3ae22b
helllth <th@cismet.de>
2012-08-31 13:55:02 +0200

    #10 preparing ... added User and Callserver provider to make it possible for a lightweightMetaObject to retrieve its full object info

---
+++ src/main/java/Sirius/server/newuser/UserContextProvider.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sirius.server.newuser;
+
+/**
+ *
+ * @author thorsten
+ */
+public interface UserContextProvider {
+    User getUser();
+}
---
+++ src/main/java/de/cismet/cids/server/CallServerServiceProvider.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cids.server;
+
+/**
+ *
+ * @author thorsten
+ */
+public interface CallServerServiceProvider {
+    CallServerService getCallServerService();
+}
