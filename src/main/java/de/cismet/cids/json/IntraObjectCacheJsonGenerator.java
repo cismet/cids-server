b4ac1074c24566d92f033d856551ba81b31efcdc
helllth <th@cismet.de>
2013-03-05 18:16:20 +0100

    working on #29. adding an intra object cache to the json cabailities of an cidsbean

--- src/main/java/de/cismet/cids/json/IntraObjectCacheJsonGenerator.java
+++ src/main/java/de/cismet/cids/json/IntraObjectCacheJsonGenerator.java
@@ -11,13 +11,20 @@
  */
 package de.cismet.cids.json;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.util.JsonGeneratorDelegate;
 
 import java.io.IOException;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 import de.cismet.cids.dynamics.CidsBean;
 
@@ -56,12 +63,14 @@ public class IntraObjectCacheJsonGenerator extends JsonGeneratorDelegate {
     public boolean containsKey(final String key) {
         return ioc.containsKey(key);
     }
+
     /**
      * DOCUMENT ME!
      */
     public void clear() {
         ioc.clear();
     }
+
     /**
      * DOCUMENT ME!
      *
@@ -72,6 +81,7 @@ public class IntraObjectCacheJsonGenerator extends JsonGeneratorDelegate {
     public CidsBean get(final String key) {
         return ioc.get(key);
     }
+
     /**
      * DOCUMENT ME!
      *
@@ -82,8 +92,103 @@ public class IntraObjectCacheJsonGenerator extends JsonGeneratorDelegate {
         ioc.put(key, bean);
     }
 
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   value  DOCUMENT ME!
+     *
+     * @throws  IOException              DOCUMENT ME!
+     * @throws  JsonProcessingException  DOCUMENT ME!
+     */
     @Override
     public void writeObject(final Object value) throws IOException, JsonProcessingException {
-        super.writeObject(value);
+        if (value == null) {
+            // important: call method that does check value write:
+            writeNull();
+        } else {
+            /* 02-Mar-2009, tatu: we are NOT to call _verifyValueWrite here,
+             *   because that will be done when codec actually serializes  contained POJO. If we did call it it would
+             * advance state  causing exception later on
+             */
+            if (getCodec() != null) {
+                getCodec().writeValue(this, value);
+                return;
+            }
+            _writeSimpleObject(value);
+        }
+    }
+
+    /**
+     * Helper method to try to call appropriate write method for given untyped Object. At this point, no structural
+     * conversions should be done, only simple basic types are to be coerced as necessary.
+     *
+     * @param   value  Non-null value to write
+     *
+     * @throws  IOException              DOCUMENT ME!
+     * @throws  JsonGenerationException  DOCUMENT ME!
+     * @throws  IllegalStateException    DOCUMENT ME!
+     */
+    protected void _writeSimpleObject(final Object value) throws IOException, JsonGenerationException {
+        /* 31-Dec-2009, tatu: Actually, we could just handle some basic
+         *    types even without codec. This can improve interoperability,   and specifically help with TokenBuffer.
+         */
+        if (value == null) {
+            writeNull();
+            return;
+        }
+        if (value instanceof String) {
+            writeString((String)value);
+            return;
+        }
+        if (value instanceof Number) {
+            final Number n = (Number)value;
+            if (n instanceof Integer) {
+                writeNumber(n.intValue());
+                return;
+            } else if (n instanceof Long) {
+                writeNumber(n.longValue());
+                return;
+            } else if (n instanceof Double) {
+                writeNumber(n.doubleValue());
+                return;
+            } else if (n instanceof Float) {
+                writeNumber(n.floatValue());
+                return;
+            } else if (n instanceof Short) {
+                writeNumber(n.shortValue());
+                return;
+            } else if (n instanceof Byte) {
+                writeNumber(n.byteValue());
+                return;
+            } else if (n instanceof BigInteger) {
+                writeNumber((BigInteger)n);
+                return;
+            } else if (n instanceof BigDecimal) {
+                writeNumber((BigDecimal)n);
+                return;
+
+                // then Atomic types
+
+            } else if (n instanceof AtomicInteger) {
+                writeNumber(((AtomicInteger)n).get());
+                return;
+            } else if (n instanceof AtomicLong) {
+                writeNumber(((AtomicLong)n).get());
+                return;
+            }
+        } else if (value instanceof byte[]) {
+            writeBinary((byte[])value);
+            return;
+        } else if (value instanceof Boolean) {
+            writeBoolean((Boolean)value);
+            return;
+        } else if (value instanceof AtomicBoolean) {
+            writeBoolean(((AtomicBoolean)value).get());
+            return;
+        }
+        throw new IllegalStateException(
+            "No ObjectCodec defined for the generator, can only serialize simple wrapper types (type passed "
+                    + value.getClass().getName()
+                    + ")");
     }
 }
