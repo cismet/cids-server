35bbb9d4f2a569e255b1f2e601e94388d3e25400
Daniel Meiers <daniel.meiers@cismet.de>
2013-05-03 09:48:19 +0200

    #44 formatting

--- src/main/java/de/cismet/cids/server/actions/UserAwareServerAction.java
+++ src/main/java/de/cismet/cids/server/actions/UserAwareServerAction.java
@@ -1,3 +1,10 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -7,10 +14,10 @@ package de.cismet.cids.server.actions;
 import Sirius.server.middleware.interfaces.domainserver.UserStore;
 
 /**
+ * DOCUMENT ME!
  *
- * @author daniel
+ * @author   daniel
+ * @version  $Revision$, $Date$
  */
 public interface UserAwareServerAction extends ServerAction, UserStore {
-
-
 }
