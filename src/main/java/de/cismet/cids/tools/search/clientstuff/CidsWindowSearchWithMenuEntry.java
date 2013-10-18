ed880a7f0df7a114c9659393dcdf1d9c53263eb5
therter <thorsten.herter@cismet.de>
2013-10-18 14:21:19 +0200

    fixed #51 introduction of the CidsWindowSearchWithMenuEntry interface

---
+++ src/main/java/de/cismet/cids/tools/search/clientstuff/CidsWindowSearchWithMenuEntry.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cids.tools.search.clientstuff;
+
+/**
+ *
+ * @author therter
+ */
+public interface CidsWindowSearchWithMenuEntry extends CidsWindowSearch {
+    
+}
