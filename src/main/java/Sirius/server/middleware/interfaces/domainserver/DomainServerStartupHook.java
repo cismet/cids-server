2dbc37fe742aead8b004e9d511f9e5a09ea1c193
Daniel Meiers <daniel.meiers@cismet.de>
2014-01-24 11:54:24 +0100

    #59 introducing new interface for the startup hooks

---
+++ src/main/java/Sirius/server/middleware/interfaces/domainserver/DomainServerStartupHook.java
@@ -0,0 +1,29 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sirius.server.middleware.interfaces.domainserver;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   daniel
+ * @version  $Revision$, $Date$
+ */
+public interface DomainServerStartupHook {
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     */
+    void domainServerStarted();
+}
