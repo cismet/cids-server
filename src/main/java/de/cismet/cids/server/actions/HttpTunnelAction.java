27785b4e12cd99d983c51a55c761bf9e54c1c665
Daniel Meiers <daniel.meiers@cismet.de>
2014-08-22 11:44:24 +0200

    #77 use HTTPAccessHandler without CredentialProvider
    since the dependency to cismet-gui-commons is no longer present, we need
    to use a HTTPAccessHandler without GUICredentialProvider in the
    HTTPTunnelAction.

--- src/main/java/de/cismet/cids/server/actions/HttpTunnelAction.java
+++ src/main/java/de/cismet/cids/server/actions/HttpTunnelAction.java
@@ -22,6 +22,7 @@ import java.util.HashMap;
 
 import de.cismet.commons.security.AccessHandler;
 import de.cismet.commons.security.exceptions.BadHttpStatusCodeException;
+import de.cismet.commons.security.handler.SimpleHttpAccessHandler;
 
 /**
  * DOCUMENT ME!
@@ -86,8 +87,7 @@ public class HttpTunnelAction implements ServerAction {
             if (LOG.isDebugEnabled()) {
                 LOG.debug("tunneled Request for:" + url + "?" + request + " (end of url)");
             }
-            final AccessHandler handler = new DefaultHTTPAccessHandler();
-//            final AccessHandler handler = new BasicHTTPAccessHandler();
+            final AccessHandler handler = new SimpleHttpAccessHandler();
             final InputStream is = handler.doRequest(
                     url,
                     new StringReader(request),
