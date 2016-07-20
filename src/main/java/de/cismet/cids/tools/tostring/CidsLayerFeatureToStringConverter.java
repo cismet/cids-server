7d485c9f47fac81e22d7b2232cce1ddd380e6ede
therter <thorsten.herter@cismet.de>
2016-07-20 10:44:22 +0200

    #50 add the toString converter for CidsLayerFeatures

---
+++ src/main/java/de/cismet/cids/tools/tostring/CidsLayerFeatureToStringConverter.java
@@ -0,0 +1,33 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cids.tools.tostring;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   therter
+ * @version  $Revision$, $Date$
+ */
+public interface CidsLayerFeatureToStringConverter {
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   feature  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    String featureToString(Object feature);
+}
