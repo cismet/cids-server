b08bbf17fd5648be3ae745db590d537ec7f20854
Pascal Dihé <pascal@cismet.de>
2016-09-23 16:36:36 +0200

    #183 convert clob to string

--- src/main/java/Sirius/server/middleware/impls/domainserver/DomainServerImpl.java
+++ src/main/java/Sirius/server/middleware/impls/domainserver/DomainServerImpl.java
@@ -40,7 +40,6 @@ import Sirius.server.sql.PreparableStatement;
 
 import org.apache.log4j.PropertyConfigurator;
 
-import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
 
 import java.io.File;
@@ -53,6 +52,7 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 
+import java.sql.Clob;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -761,14 +761,30 @@ public class DomainServerImpl extends UnicastRemoteObject implements CatalogueSe
      *
      * @return  DOCUMENT ME!
      *
-     * @throws  SQLException  DOCUMENT ME!
+     * @throws  SQLException           DOCUMENT ME!
+     * @throws  IllegalStateException  DOCUMENT ME!
      */
     public ArrayList<ArrayList> collectResults(final ResultSet rs) throws SQLException {
         final ArrayList<ArrayList> result = new ArrayList<ArrayList>();
         while (rs.next()) {
             final ArrayList row = new ArrayList();
             for (int i = 0; i < rs.getMetaData().getColumnCount(); ++i) {
-                row.add(rs.getObject(i + 1));
+                final Object objectFromResultSet = rs.getObject(i + 1);
+
+                if (objectFromResultSet instanceof Clob) {
+                    // we convert the clob to a string, otherwise the value is not serialisable out of the
+                    // box due to the direct connection to the database
+                    // TODO: handle overflows, i.e. clob too big
+                    final Clob clob = (Clob)objectFromResultSet;
+                    if (clob.length() <= Integer.valueOf(Integer.MAX_VALUE).longValue()) {
+                        row.add(clob.getSubString(1, Long.valueOf(clob.length()).intValue()));
+                    } else {
+                        throw new IllegalStateException(
+                            "cannot handle clobs larger than Integer.MAX_VALUE)");
+                    }
+                } else {
+                    row.add(objectFromResultSet);
+                }
             }
             result.add(row);
         }
