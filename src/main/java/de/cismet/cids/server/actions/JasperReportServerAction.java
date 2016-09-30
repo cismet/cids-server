a50937f7e8e759d1f286e63f85bb296a2390b8d2
JeanAtCismet <jean.ruiz@cismet.de>
2016-09-30 12:55:36 +0200

    fixed #184 JasperReportServerAction implemented

---
+++ src/main/java/de/cismet/cids/server/actions/JasperReportServerAction.java
@@ -0,0 +1,106 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cids.server.actions;
+
+import Sirius.server.middleware.interfaces.domainserver.MetaService;
+import Sirius.server.middleware.interfaces.domainserver.MetaServiceStore;
+import Sirius.server.newuser.User;
+
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+
+import org.apache.log4j.Logger;
+
+import java.io.ByteArrayOutputStream;
+
+import java.util.Map;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   jruiz
+ * @version  $Revision$, $Date$
+ */
+public abstract class JasperReportServerAction implements UserAwareServerAction, MetaServiceStore {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    protected static final transient Logger LOG = Logger.getLogger(JasperReportServerAction.class);
+
+    //~ Instance fields --------------------------------------------------------
+
+    private User user;
+    private MetaService metaService;
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   parameters  DOCUMENT ME!
+     * @param   dataSource  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     *
+     * @throws  Exception  DOCUMENT ME!
+     */
+    protected byte[] generateReport(final Map<String, Object> parameters, final JRDataSource dataSource)
+            throws Exception {
+        final JasperReport report = (JasperReport)JRLoader.loadObject(
+                JasperReportServerAction.class.getResourceAsStream(getJasperPath()));
+        final JasperPrint print = JasperFillManager.fillReport(report, parameters, dataSource);
+
+        ByteArrayOutputStream os = null;
+        try {
+            os = new ByteArrayOutputStream();
+            JasperExportManager.exportReportToPdfStream(print, os);
+            final byte[] bytes = os.toByteArray();
+            return bytes;
+        } finally {
+            if (os != null) {
+                os.close();
+            }
+        }
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    protected abstract String getJasperPath();
+
+    @Override
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public void setUser(final User user) {
+        this.user = user;
+    }
+
+    @Override
+    public void setMetaService(final MetaService metaService) {
+        this.metaService = metaService;
+    }
+
+    @Override
+    public MetaService getMetaService() {
+        return metaService;
+    }
+}
