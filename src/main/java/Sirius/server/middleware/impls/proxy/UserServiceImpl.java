8432f04f84607c4b41d0cdcdeb6ce40a1c32610b
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2021-04-26 16:37:23 +0200

    fixed #241 UserService modified, so that the jwstokens now can be generated and validated in DomainServerImpl

--- src/main/java/Sirius/server/middleware/impls/proxy/UserServiceImpl.java
+++ src/main/java/Sirius/server/middleware/impls/proxy/UserServiceImpl.java
@@ -21,9 +21,6 @@ import Sirius.server.newuser.UserException;
 import Sirius.server.newuser.UserGroup;
 import Sirius.server.newuser.UserServer;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Encoders;
@@ -48,8 +45,6 @@ import javax.crypto.spec.SecretKeySpec;
 
 import javax.management.ObjectName;
 
-import de.cismet.cids.server.connectioncontext.ConnectionContextManagement;
-
 import de.cismet.connectioncontext.ConnectionContext;
 
 /**
@@ -64,7 +59,6 @@ public class UserServiceImpl {
 
     private static final transient Logger LOG = Logger.getLogger(UserServiceImpl.class);
     private static final String DOMAINSPLITTER = "@";
-    private static byte[] decodedKey = Base64.getDecoder().decode(createRandomKey());
 
     //~ Instance fields --------------------------------------------------------
 
@@ -87,24 +81,6 @@ public class UserServiceImpl {
     }
 
     //~ Methods ----------------------------------------------------------------
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     */
-    private static String createRandomKey() {
-        final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-
-        return Encoders.BASE64.encode(key.getEncoded());
-    }
-
-    /**
-     * DOCUMENT ME!
-     */
-    public static void recreateRandomKey() {
-        decodedKey = Base64.getDecoder().decode(createRandomKey());
-    }
 
     /**
      * DOCUMENT ME!
@@ -154,12 +130,12 @@ public class UserServiceImpl {
      * Wie konnte das jemals gehen Falsche Reihenfolge in Signatur public User getUser( String userLsName, String
      * userName, String userGroupLsName, String userGroupName, String password) throws RemoteException, UserException {.
      *
-     * @param   userGroupLsName  DOCUMENT ME!
-     * @param   userGroupName    DOCUMENT ME!
-     * @param   userLsName       DOCUMENT ME!
-     * @param   userName         DOCUMENT ME!
-     * @param   password         DOCUMENT ME!
-     * @param   context          DOCUMENT ME!
+     * @param   userGroupLsName    DOCUMENT ME!
+     * @param   userGroupName      DOCUMENT ME!
+     * @param   userLsName         DOCUMENT ME!
+     * @param   userName           DOCUMENT ME!
+     * @param   password           DOCUMENT ME!
+     * @param   connectionContext  DOCUMENT ME!
      *
      * @return  DOCUMENT ME!
      *
@@ -171,10 +147,7 @@ public class UserServiceImpl {
             final String userLsName,
             final String userName,
             final String password,
-            final ConnectionContext context) throws RemoteException, UserException {
-        final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-        final Key serverKey = new SecretKeySpec(decodedKey, signatureAlgorithm.getJcaName());
-
+            final ConnectionContext connectionContext) throws RemoteException, UserException {
         if (LOG.isDebugEnabled()) {
             LOG.debug("getUser calles for user::" + userName); // NOI18N
 
@@ -185,59 +158,37 @@ public class UserServiceImpl {
             LOG.debug((("password:" + password) == null) ? "null" : "*****"); // NOI18N
         }
 
-        final User u;
+        final Sirius.server.middleware.interfaces.domainserver.UserService us =
+            (Sirius.server.middleware.interfaces.domainserver.UserService)activeLocalServers.get(userLsName);
+        if (us == null) {
+            throw new UserException(
+                "Login failed, home server of the user is not reachable :: "
+                        + userName, // NOI18N
+                false,
+                false,
+                false,
+                true);
+        }
+
+        User validatedUser = null;
         if (userName.equals("jwt")) {
             final String jwt = password;
-            final Claims claims = Jwts.parserBuilder().setSigningKey(serverKey).build().parseClaimsJws(jwt).getBody();
-            final int uid = new Integer(claims.getId()).intValue();
-            final String jwtUsername = claims.getSubject();
-            final String jwtDomain = claims.get("domain", String.class);
-            final String jwtUsergroup = claims.get("usergroup", String.class);
-            final String jwtUsergroupDomain = claims.get("usergroupDomain", String.class);
-            u = userServer.getUser(jwtUsergroupDomain, jwtUsergroup, jwtDomain, jwtUsername, "jwtCreatedUser");
-            u.setValid();
-            return u;
+            validatedUser = us.validateUser(jwt, connectionContext);
         } else {
-//
-
-            u = userServer.getUser(userGroupLsName, userGroupName, userLsName, userName, password);
-
-            boolean validated = false;
-
-            if (u != null) {
-                final Sirius.server.middleware.interfaces.domainserver.UserService us =
-                    (Sirius.server.middleware.interfaces.domainserver.UserService)activeLocalServers.get(userLsName);
-
-                if (us != null) {
-                    validated = us.validateUser(u, password, context);
-                } else {
-                    throw new UserException(
-                        "Login failed, home server of the user is not reachable :: "
-                                + userName, // NOI18N
-                        false,
-                        false,
-                        false,
-                        true);
-                }
+            final User user = userServer.getUser(userGroupLsName, userGroupName, userLsName, userName, password);
+            if (user != null) {
+                validatedUser = us.validateUser(user, password, connectionContext);
             }
-
-            if (validated) {
-                final String loginRestrictionValue = getConfigAttr(u, "login.restriction", context);
-                if (loginRestrictionValue != null) {
-                    LoginRestrictionHelper.getInstance().checkLoginRestriction(loginRestrictionValue);
-                }
-
-                final JwtBuilder builder = Jwts.builder().setId(u.getId() + "").setSubject(userName);
-                builder.claim("domain", userLsName);
-                if ((userGroupName != null) && (userGroupLsName != null)) {
-                    builder.claim("usergroup", userGroupName);
-                    builder.claim("usergroupDomain", userGroupLsName);
-                }
-
-                final String jws = builder.signWith(serverKey).compact();
-                u.setJwsToken(jws);
-                return u;
+        }
+        if (validatedUser != null) {
+            final String loginRestrictionValue = getConfigAttr(
+                    validatedUser,
+                    "login.restriction",
+                    connectionContext);
+            if (loginRestrictionValue != null) {
+                LoginRestrictionHelper.getInstance().checkLoginRestriction(loginRestrictionValue);
             }
+            return validatedUser;
         }
         throw new UserException("Login failed :: " + userName, false, true, false, false); // NOI18N
     }
