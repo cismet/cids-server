3fe1dc35c81e82be2f1e592a64690c8a46cfb0dd
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2021-05-04 08:27:04 +0200

    fixed #243

---
+++ src/main/java/de/cismet/cids/utils/serverresources/ServerResourcePropertiesHandler.java
@@ -0,0 +1,33 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cids.utils.serverresources;
+
+import java.util.Properties;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   jruiz
+ * @version  $Revision$, $Date$
+ */
+public interface ServerResourcePropertiesHandler {
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  properties  DOCUMENT ME!
+     */
+    void setProperties(final Properties properties);
+}
