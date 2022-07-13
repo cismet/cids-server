3c43c66e2fd5a5e37d7ca7d65dfad5e7d1f5dc7c
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2022-07-13 11:56:47 +0200

    Revert "Revert "issue #273 fixing resultSet not closing""
    This reverts commit 7198adae6609e87508d27c4bdc64e4d76724db99.

--- src/main/java/Sirius/server/localserver/user/UserStore.java
+++ src/main/java/Sirius/server/localserver/user/UserStore.java
@@ -122,6 +122,7 @@ public final class UserStore extends Shutdown {
                 }
             }
 
+            
             userGroupTable.close();
 
             // --------------------load memberships--------------------------------------------------
